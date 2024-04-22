0b7edd6f39cb82b73066c9696ed07b6dfb59e5c8
supportPsoft <68959708+supportPsoft@users.noreply.github.com>
2024-04-22 16:03:32 +0200

    allow headers cors2

--- middleware/cors2.js
+++ middleware/cors2.js
@@ -1,11 +1,14 @@
 class corsPolicy {
     constructor() {}
 
-    init(config,ctxt) {
+    init(config,ctxt) 
+    {
+        let allowHeaders = config.allowed_headers || "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Authorization, X-CLIENT-ID, gtoken, Content-Length";
+
         /* ================ CORS ================== */
         ctxt.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Authorization, gtoken, Content-Length");
+            res.header("Access-Control-Allow-Headers", allowHeaders);
             res.header("Access-Control-Allow-Credentials", "true");
             next();
         });
